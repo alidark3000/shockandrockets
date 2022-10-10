3f429f090c10a9d80767be633849cc8e8cdc54e6
Alistair Dark <alistairdark@Alistairs-MB-M1.local>
2022-10-10 14:29:42 +1000

    update

---
+++ wp-content/themes/blankslate/footer.php
@@ -0,0 +1,26 @@
+</main>
+<?php get_sidebar(); ?>
+</div>
+<footer id="footer" role="contentinfo">
+<div id="copyright">
+<p><a rel="license" rel="nofollow" href="http://creativecommons.org/licenses/by-sa/4.0/"></a><br />This work is licensed under a <a rel="license" rel="nofollow" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.</p>
+	<p>
+		This website runs on <a href="https://wordpress.org" rel="nofollow" target="_blank">WordPress</a>, using a custom theme based on <a href="https://en-au.wordpress.org/themes/blankslate/" rel="nofollow" target="_blank">BlankSlate</a>, with static files genereated by <a href="https://wp2static.com/" rel="nofollow" target="_blank">WP2Static</a> and hosted with <a href="https://pages.github.com/" rel="nofollow" target="_blank">GitHub pages</a>. 
+	</p>
+	<p>
+		<a href="https://www.youtube.com/user/alextababa">YouTube</a><span class="meta-sep">|</span>
+	
+	
+<a href="Shock & Rockets Discord">Discord</a><span class="meta-sep">|</span>
+		<a href="https://www.twitch.tv/directory/game/Glitch%20Arena/clips?range=7d">Twitch</a><span class="meta-sep">|</span>
+		<a href="https://store.steampowered.com/app/1083800/Glitch_Arena/">Glitch on Steam</a><span class="meta-sep">|</span>
+		<a href="https://alidark3000.notion.site/Glitch-Arena-Manual-688340c49e974e96ada14b6f00de6219">Glitch Manual</a>
+		
+	</p>
+</div>
+</footer>
+</div>
+<?php wp_footer(); ?>
+
+</body>
+</html>
