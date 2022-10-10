c659c507c53c5e041b5a00acb5a0e2d49654e0d6
Alistair Dark <alistairdark@Alistairs-MB-M1.local>
2022-10-10 14:07:28 +1000

    upoad site

---
+++ wp-content/themes/blankslate/footer.php
@@ -0,0 +1,20 @@
+</main>
+<?php get_sidebar(); ?>
+</div>
+<footer id="footer" role="contentinfo">
+<div id="copyright">
+<p><a rel="license" rel="nofollow" href="http://creativecommons.org/licenses/by-sa/4.0/"></a><br />This work is licensed under a <a rel="license" rel="nofollow" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.</p>
+	<p>
+		This website runs on <a href="https://wordpress.org" rel="nofollow" target="_blank">WordPress</a>, using a custom theme based on <a href="https://en-au.wordpress.org/themes/blankslate/" rel="nofollow" target="_blank">BlankSlate</a>, with static files genereated by <a href="https://wp2static.com/" rel="nofollow" target="_blank">WP2Static</a> and hosted with <a href="https://pages.github.com/" rel="nofollow" target="_blank">GitHub pages</a>. 
+	</p>
+	<p>
+		<a href="https://www.youtube.com/channel/UC2ckGHLPjZ5sN4cR31SsT_g">YouTube</a><span class="meta-sep">|</span><a href="https://www.instagram.com/alidark3000/">Instagram</a><span class="meta-sep">|</span><a href="https://twitter.com/alidark3000">Twitter</a><span class="meta-sep">|</span>
+		<a href="https://vsco.co/alidark3000/gallery">VSCO</a>
+	</p>
+</div>
+</footer>
+</div>
+<?php wp_footer(); ?>
+
+</body>
+</html>
